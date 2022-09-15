1197f9662a10249c7c453fe534cba7b7957a1b47
diana-atas <dianadavidatas@gmail.com>
2022-09-15 16:12:03 +1200

    move searchappbar from home to app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import TourCard from '../components/TourCard'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid' // Grid version 1
-import SearchAppBar from '../components/AppBar'
+
 // import Grid2 from '@mui/material/Unstable_Grid2' // Grid version 2
 import cities from '../data.json'
 import { Typography } from '@mui/material'
 
 const Home = () => (
   <div className="App">
-    <SearchAppBar />
     <Container sx={{ marginY: 5 }}>
       {cities.map((city) => (
         <div key={city.name}>
